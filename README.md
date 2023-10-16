5dd0e0ca6afd31ce0c5e22f3b10c26c10af39a3f
Gonzalo Cavazos <gonzalo.cavazos@epicor.com>
2023-10-16 08:33:11 -0600

    AnyList scaffold

---
+++ README.md
@@ -0,0 +1,8 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+1. Clone project git clone <repo>
+2. Create .env.example to .env and configure
+3. docker compose up -d
+4. npm run start:dev
---
+++ src/app.module.ts
@@ -0,0 +1,39 @@
+import { ConfigModule } from '@nestjs/config';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // debug: false,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault()]
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities:true,
+      entities: [],
+      synchronize: true,
+    }),
+
+    ItemsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
